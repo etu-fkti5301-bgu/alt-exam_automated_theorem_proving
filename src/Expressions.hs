48efbd91baac583b48e419e7daf1f28211a4842c
Deligor6321 <deligor6321@gmail.com>
2016-11-12 21:54:11 +0000

    Add simplification function

--- src/Expressions.hs
+++ src/Expressions.hs
@@ -1,9 +1,25 @@
 module Expressions
-    ( Expression(..)
+    ( Expression(..),
+      simplify
     ) where
 
 -- Expressions constructor [p. 14]
 data Expression = Var String | Const Int | Add Expression Expression | Mul Expression Expression deriving(Show)
 
+-- Functions for common simplification [p. 15]
+-- Main recursive function
+simplify :: Expression -> Expression
+simplify (Add exp1 exp2) = simplify' $ Add (simplify exp1) (simplify exp2)
+simplify (Mul exp1 exp2) = simplify' $ Mul (simplify exp1) (simplify exp2)
+simplify exp = simplify' exp
 
-
+-- Helper
+simplify' :: Expression -> Expression
+simplify' (Add (Const a) (Const b)) = Const(a + b)
+simplify' (Add (Const 0) x) = x
+simplify' (Add x (Const 0)) = x
+simplify' (Mul (Const 0) x) = Const 0
+simplify' (Mul x (Const 0)) = Const 0
+simplify' (Mul (Const 1) x) = x
+simplify' (Mul x (Const 1)) = x
+simplify' exp = exp
