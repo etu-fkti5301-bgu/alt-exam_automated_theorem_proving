0a3a1bae561f5304e1d8d98dce87f52d62de1819
Deligor6321 <deligor6321@gmail.com>
2016-12-25 18:20:10 +0000

    Add lexer and parser

---
+++ src/Util/Parse.hs
@@ -0,0 +1,13 @@
+
+module Util.Parse
+  ( module Text.ParserCombinators.Parsec
+  , module Text.ParserCombinators.Parsec.Expr
+  , module Util.Parse.Parse
+  ) 
+where
+
+import Util.Parse.Parse
+import Text.ParserCombinators.Parsec hiding (parse)
+import Text.ParserCombinators.Parsec.Expr 
+
+
