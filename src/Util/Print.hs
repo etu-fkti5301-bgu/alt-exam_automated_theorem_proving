70fc0d15e33fdd988d08310ca667947995618332
Anon <jew.bilkis@gmail.com>
2016-12-25 18:20:07 +0300

    add changes from the last commit

---
+++ src/Util/Print.hs
@@ -0,0 +1,13 @@
+
+-- Signature
+
+module Util.Print 
+  ( module Text.PrettyPrint.HughesPJ
+  , module ATP.Util.Print.Print
+  ) 
+where
+
+-- Imports
+
+import ATP.Util.Print.Print 
+import Text.PrettyPrint.HughesPJ
