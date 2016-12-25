6fd304449710acb681f16d5d3a241314f96bb265
Deligor6321 <deligor6321@gmail.com>
2016-12-25 16:25:28 +0000

    utils: Add LHStoHS script

---
+++ utils/LHStoHS.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import sys
+import os.path
+
+if len(sys.argv) < 2:
+    print('Give me a file! }:C')
+    exit(0)
+
+input_file_name = str(sys.argv[1])
+if len(input_file_name) < 4 or input_file_name[-4:] != '.lhs':
+     print('I don\'t like this file')
+else:
+     output_file_name = input_file_name[:-4] + '.hs'
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+if not os.path.isfile(input_file_name):
+    if os.path.isfile(dir_path + '\\' + input_file_name):
+        input_file_name = dir_path + '\\' + input_file_name
+        output_file_name = dir_path + '\\' + output_file_name
+    else:
+        print('I can\'t see this shit')
+        exit(0)
+
+input_file = open(input_file_name, 'r')
+output_file = open(output_file_name, 'w')
+
+lines = input_file.readlines()
+
+for line in lines:
+    if len(line) <= 1:
+        output_file.write(line)
+    elif line[0] == '>':
+        output_file.write(line[2:])
+    else:
+        output_file.write('-- ' + line[2:])
+        
+
+print('Nya! :3')
+
+input_file.close()
+output_file.close()
+
