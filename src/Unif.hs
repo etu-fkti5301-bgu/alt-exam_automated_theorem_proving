4fd7f8775e9d9074f478e42f0a81cfbc550b431d
Deligor6321 <deligor6321@gmail.com>
2016-12-26 23:12:00 +0000

    unif: Fix indents

--- src/Unif.hs
+++ src/Unif.hs
@@ -37,30 +37,31 @@ unify env eqs = case eqs of
   (Var x, t) : rest -> unifyVar x t rest
   (t, Var x) : rest -> unifyVar x t rest
   _ -> Nothing
-  where unifyVar x t rest = 
-    case Map.lookup x env of
-      Just t' -> unify env ((t', t) : rest)
-      Nothing -> case istriv env x t of
-                  Just True -> unify env rest
-                  Just False -> unify ((x |-> t) env) rest
-                  Nothing -> Nothing
+  where unifyVar x t rest = case Map.lookup x env of
+                              Just t' -> unify env ((t', t) : rest)
+                              Nothing -> case istriv env x t of
+                                          Just True -> unify env rest
+                                          Just False -> unify ((x ↦ t) env) rest
+                                          Nothing -> Nothing
 
 solve :: Env -> Maybe Env
 solve env = solve' env (Map.toList env)
 
 solve' :: Env -> [(Var, Term)] -> Maybe Env
 solve' env fs = 
-  if any (\(x,t) -> elem x (Fol.fv t)) fs then Nothing else 
-    let env' = foldr (\(x,t) -> Map.insert x (Fol.apply env t)) Map.empty fs
-  fs' = Map.toList env' in
-    if fs == fs' then Just env else solve' env' fs'
+  if any (\(x,t) -> elem x (Fol.fv t)) fs
+  then Nothing
+  else let env' = foldr (\(x,t) -> Map.insert x (Fol.apply env t)) Map.empty fs
+           fs' = Map.toList env'
+       in if fs == fs'
+          then Just env
+          else solve' env' fs'
 
 fullunify :: [(Term, Term)] -> Maybe Env
 fullunify eqs = do env <- unify Map.empty eqs 
                    solve env
 
 unifyAndApply :: [(Term, Term)] -> Maybe [(Term, Term)] 
-unifyAndApply eqs = 
-  do env <- fullunify eqs 
-    let apply (t1, t2) = (Fol.apply env t1, Fol.apply env t2) in
-      return $ map apply eqs
+unifyAndApply eqs = do env <- fullunify eqs 
+                       return $ let apply' (t1, t2) = (apply env t1, apply env t2)
+                                in map apply' eqs
