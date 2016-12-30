6dea4929e970d42aea69d7c301826345af092232
Deligor6321 <deligor6321@gmail.com>
2016-12-30 14:52:40 +0000

    Add formulas JSON creation

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,40 @@
 module Main where
 
-import Prelude
+-- Imports
+
+import Util.Prelude
+import Util.Parse
+import Formula
+import FormulaSyn
+import System.Environment
+import Skolem
+
+-- Replace double-quotes (") to (\')
+showToJSON :: Show a => a -> String
+showToJSON = foldr (\x acc -> if x == '\"' then "\\\'" ++ acc else x:acc) [] . show
+
+-- Make JSON object {"<s1>":"<s2>"}
+makeJSONObject :: String -> String -> String
+makeJSONObject s1 s2 = "\"" ++ s1 ++ "\":\"" ++ s2 ++ "\""
+
+-- Make JSON with pnf, snf
+makeJSON :: Formula -> String
+makeJSON f = let body = formula ++ "," ++ parsed ++ "," ++ inPnf ++ "," ++ inSnf
+                 formula = makeJSONObject "formula" $ showToJSON f
+                 parsed  = makeJSONObject "parsed" $ showToJSON $ pPrint f
+                 inPnf   = makeJSONObject "pnf" $ showToJSON $ pPrint $ pnf f
+                 inSnf   = makeJSONObject "snf" $ showToJSON $ pPrint $ skolemize f
+             in "{" ++ body ++  "}"
+
+-- Print JSON of each formula
+printJSONs :: [Formula] -> IO ()
+printJSONs []   = return ()
+printJSONs (x:xs) = do
+   putStrLn $ makeJSON x
+   printJSONs xs
 
 main :: IO ()
-main = putStrLn "Hello!"
+main = do
+   args <- getArgs
+   let formulas :: [Formula] = map parse args
+   printJSONs formulas
