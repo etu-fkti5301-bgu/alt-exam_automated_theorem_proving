741c4fa49d36d1ba353d093910a62853b6e56c49
andixlm <andixev@gmail.com>
2017-01-02 15:05:15 +0300

    main: Implement nnf and snf handlers

--- app/Main.hs
+++ app/Main.hs
@@ -18,11 +18,23 @@ main = do
    let mode = head args
        formulas :: [Formula] = map parse $ tail args
    case mode of
+     "nnf" -> putStrLn jsonStr
+              where
+                jsonStr = jsonToString json
+                json = makeJSON [("parsed_formulas", pforms), ("nnfs", nnfs)]
+                pforms = makeJSON $ map (makeJSON . show . pPrint) formulas
+                nnfs = makeJSON $ map (makeJSON . show . pPrint . nnf) formulas
      "pnf" -> putStrLn jsonStr
               where
                 jsonStr = jsonToString json
                 json = makeJSON [("parsed_formulas", pforms), ("pnfs", pnfs)]
                 pforms = makeJSON $ map (makeJSON . show . pPrint) formulas
                 pnfs = makeJSON $ map (makeJSON . show . pPrint . pnf) formulas
+     "snf" -> putStrLn jsonStr
+              where
+                jsonStr = jsonToString json
+                json = makeJSON [("parsed_formulas", pforms), ("snfs", snfs)]
+                pforms = makeJSON $ map (makeJSON . show . pPrint) formulas
+                snfs = makeJSON $ map (makeJSON . show . pPrint . skolemize) formulas
      _ -> putStrLn "undefined"
 
